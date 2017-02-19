fae59d2566237f6dd39b7a2218d15f1a58274180
txu <txu@godaddy.com>
2017-02-19 11:02:13 -0800

    baisc component layout

--- src/Root.js
+++ src/Root.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import Index from './components/Index';
-import ContactDetail from './components/ContactDetail';
+// import ContactDetail from './components/ContactDetail';
 
 import App from './components/App';
 
@@ -17,11 +17,11 @@ class Root extends Component {
       <Router history={this.props.history}>
         <Route path='/' component={App}>
           <IndexRoute component={Index}/>
-          <Route path='/contact/:id' component={ContactDetail} />
         </Route>
       </Router>
     );
   }
 }
 
+// <Route path='/contact/:id' component={ContactDetail} />
 export default Root;
